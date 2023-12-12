7c14751dc8f71f0ed4f371315677bd56ace7d292
AndresLaraV <aslarav@uce.edu.ec>
2023-12-11 19:45:15 -0500

    Taller14

---
+++ src/main/java/com/uce/edu/Pa2U2P5AlApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5AlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5AlApplication.class, args);
+	}
+
+}
